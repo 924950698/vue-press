862ce8e8d3fc386e76997ae84c39cafdd5b49963
liuxiangdong <www.924950698@qq.com>
2019-06-11 00:03:56 +0800

    commit 2.1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
   	repo: '924950698',// 默认是 false, 设置为 true 来启用，右上角会出现 github 跳转链接
   	lastUpdated: 'Last Updated', // 每个文档展示最近更新时间
   	nav: [
-      { text: '主页', link: '/guide/install/install' }
+      { text: '主页', link: '/' }
     ],
     sidebar:{
       '/guide/': [
@@ -17,19 +17,11 @@ module.exports = {
           title: '入门',
           collapsable: false,
           children: [      // 子菜单
-            'install/install',   // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
+            'install/test',   // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
           ]
-        },
-        {
-          title: 'JavaScript',
-          collapsable: false,
-          children: [ 
-            'javascript/javascript', 
-          ]
-        },
+        }
       ]
     } 
-   
   },
   markdown: { // markdown 插入代码时展示对应行数
     lineNumbers: true
