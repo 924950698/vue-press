dc7cc08f869e644fd9d4cef0f45ca5e51709e11d
liuxiangdong <www.924950698@qq.com>
2019-06-10 22:44:18 +0800

    commit 1.6

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,15 +16,14 @@ module.exports = {
         title: '入门',
         collapsable: false,
         children: [      // 子菜单
-          '/install/install',   // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
+          '/guide/install/install',   // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
         ]
       },
       {
         title: 'JavaScript',
         collapsable: false,
         children: [ 
-          '/javascript/', 
-          '/javascript1/', 
+          '/guide/javascript/javascript', 
         ]
       },
     ]
