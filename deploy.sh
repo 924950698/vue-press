0aadf4a42ebd8437486e1cb55c50541be5c9d5ce
liuxiangdong <www.924950698@qq.com>
2019-06-10 17:28:59 +0800

    commit 6

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+#git push -f git@github.com:924950698/vue-press.git master:gh-pages
 
 cd -
