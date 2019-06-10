0b10982a3a076e85883532ed5893c4c816a5e960
liuxiangdong <[924950698@qq.com]>
2019-06-10 23:55:31 +0800

    commit 2.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,22 +11,24 @@ module.exports = {
   	nav: [
       { text: '主页', link: '/guide/install/install' }
     ],
-    sidebar: [
-      {
-        title: '入门',
-        collapsable: false,
-        children: [      // 子菜单
-          '/guide/install/install',   // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
-        ]
-      },
-      {
-        title: 'JavaScript',
-        collapsable: false,
-        children: [ 
-          '/guide/javascript/javascript', 
-        ]
-      },
-    ]
+    sidebar:{
+      '/guide/': [
+        {
+          title: '入门',
+          collapsable: false,
+          children: [      // 子菜单
+            'install/install',   // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
+          ]
+        },
+        {
+          title: 'JavaScript',
+          collapsable: false,
+          children: [ 
+            'javascript/javascript', 
+          ]
+        },
+      ]
+    } 
    
   },
   markdown: { // markdown 插入代码时展示对应行数
