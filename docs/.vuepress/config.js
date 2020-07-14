176cf41959ba58f10202d99cd1ded07f53902849
liuxiangdong3@xdf.cn <liuxiangdong3@xdf.cn>
2020-07-14 17:34:03 +0800

    feat: 解决冲突

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -94,6 +94,12 @@ module.exports = {
               'Docker/docker1',
             ]
         },{
+          title: '小程序篇',
+          collapsable: false,
+          children: [
+              'miniProgram/taro.md',
+          ]
+      },{
           title: '正则篇',
           collapsable: false,
           children: [  
@@ -111,108 +117,6 @@ module.exports = {
             'jottings/gitHub',
           ]
         },
-    themeConfig: {
-        repo: '924950698',// 默认是 false, 设置为 true 来启用，右上角会出现 github 跳转链接
-        lastUpdated: 'Last Updated', // 每个文档展示最近更新时间
-        nav: [
-            { text: '主页', link: '/' }
-        ],
-        sidebar: {
-            '/guide/': [
-                {
-
-                    title: 'JavaScript篇',
-                    collapsable: false,
-                    children: [
-                        'JavaScript/data',
-                    ]
-                }, {
-                    title: '前端性能篇',
-                    collapsable: false,
-                    children: [
-                        'performanceOptimization/foreword.md',
-                        'performanceOptimization/netWork1.md',
-                        'performanceOptimization/netWork2.md',
-                        'performanceOptimization/localstorage1.md', // Http优化
-                        'performanceOptimization/localstorage2.md', // 本地存储优化
-                        'performanceOptimization/cdn.md',
-                        'performanceOptimization/application1.md',
-                        'performanceOptimization/application2.md',
-                        'performanceOptimization/render1.md',
-                        'performanceOptimization/render2.md',
-                        // 'performanceOptimization/render3.md',
-                        // 'performanceOptimization/render4.md',
-                    ]
-                }, {
-                    title: '小程序篇',
-                    collapsable: false,
-                    children: [
-                        'miniProgram/taro.md',
-                    ]
-                }, {
-                    title: 'Angular篇',
-                    collapsable: false,
-                    children: [
-                        'angular/install.md',
-                        'angular/angular1.md',
-                        'angular/angular2.md',
-                        'angular/angular3.md',
-                        'angular/angular4.md',
-                        'angular/angular5.md',
-                        // 'angular/angular6.md',
-                        'angular/angular7.md',
-                        'angular/angular8.md',
-                    ]
-                }, {
-                    title: '前端自动化篇',
-                    collapsable: false,
-                    children: [
-                        'autoTest/lesson1',
-                    ]
-                }, {
-                    title: 'React篇',
-                    collapsable: false,
-                    children: [
-                        'React/React-SSR', //React服务端渲染   
-                        // 'React/baseSkill',  //React基础技能篇
-                        // 'React/trialError', //React试错篇
-                        // 'React/hooks'       //React-Hooks篇  
-                    ]
-                }, {
-                    title: '算法篇',
-                    collapsable: false,
-                    children: [
-                        'arithmetic/array'
-                    ]
-                }, {
-                    title: 'typeScript篇',
-                    collapsable: false,
-                    children: [
-                        'typeScript/foreword.md',
-                        'typeScript/types.md',
-                        'typeScript/interface.md',
-                        'typeScript/class.md',
-                        'typeScript/function.md',
-                    ]
-                }, {
-                    title: '正则篇',
-                    collapsable: false,
-                    children: [
-                        'regexp/Regexp',
-                        'regexp/Regexp1',
-                        'regexp/Regexp2',
-                    ]
-                }, {
-                    title: '杂记录篇',
-                    collapsable: false,
-                    children: [
-                        'jottings/https', //网络协议篇
-                        'jottings/fire',  // 火焰图
-                        'jottings/gitLab',
-                        'jottings/gitHub',
-                        // 'jottings/write',
-                    ]
-                },
 
 
                 // {
@@ -231,8 +135,7 @@ module.exports = {
                 //   ]
                 // },
             ]
-        }
-    },
+        },
     markdown: {  // markdown 插入代码时展示对应行数
         lineNumbers: true
     }
